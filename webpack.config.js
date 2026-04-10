1609a2ef0bafe9d906548100998543dbff8e3e85
churit <churit@naver.com>
2026-04-10 22:56:57 +0900

    Add GitHub Pages deployment config

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,6 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+  config.output.publicPath = '/whiteMaker/';
   return config;
 };
